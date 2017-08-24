4cb311aa62386500083e9b646ff5b76a825ee6d8
Mel McCalla <melmccalla@gmail.com>
2017-08-24 16:38:22 -0700

    Added tests for isDimension() and parseDimension()

--- test/HTML/Microsyntaxes/Numbers/Dimension_test.cpp
+++ test/HTML/Microsyntaxes/Numbers/Dimension_test.cpp
@@ -19,6 +19,7 @@
  *******************************************************************************/
 
 #include <HTML/Microsyntaxes/Numbers/Dimension.hpp>
+#include <HTML/Microsyntaxes/Numbers/NumbersTypes.hpp>
 
 #include <gtest/gtest.h>
 
@@ -28,11 +29,59 @@ namespace Microsyntaxes {
 namespace Numbers {
 
 TEST(HTML_Microsyntaxes_Numbers_Dimension, isDimension) {
-
+	EXPECT_TRUE(isDimension("1"));
+	EXPECT_TRUE(isDimension("01"));
+	EXPECT_TRUE(isDimension("010"));
+	EXPECT_TRUE(isDimension("1%"));
+	EXPECT_TRUE(isDimension("1.0"));
+	EXPECT_TRUE(isDimension("1.0%"));
+	EXPECT_TRUE(isDimension("01.0%"));
+	EXPECT_TRUE(isDimension("0001.0%"));
+	EXPECT_TRUE(isDimension("0001.0"));
+	EXPECT_TRUE(isDimension("0001"));
+	EXPECT_TRUE(isDimension("0002"));
+	EXPECT_FALSE(isDimension("a"));
+	EXPECT_FALSE(isDimension("0a1"));
+	EXPECT_FALSE(isDimension("01.a"));
+	EXPECT_FALSE(isDimension("0.1&"));
+	EXPECT_FALSE(isDimension("0.1.1"));
+	EXPECT_FALSE(isDimension("00000"));
+	EXPECT_FALSE(isDimension("0"));
+	EXPECT_FALSE(isDimension("%"));
+	EXPECT_FALSE(isDimension("."));
+	EXPECT_FALSE(isDimension("1.0e10"));
+	EXPECT_FALSE(isDimension("0.1"));
+	EXPECT_FALSE(isDimension("0.1%"));
+	EXPECT_FALSE(isDimension("0."));
+	EXPECT_FALSE(isDimension(".0"));
 }
 
 TEST(HTML_Microsyntaxes_Numbers_Dimension, parseDimension) {
-
+	EXPECT_EQ(Dimension(1, DimensionType::LENGTH), parseDimension("1"));
+	EXPECT_EQ(Dimension(1, DimensionType::LENGTH), parseDimension("01"));
+	EXPECT_EQ(Dimension(10, DimensionType::LENGTH), parseDimension("010"));
+	EXPECT_EQ(Dimension(1, DimensionType::PERCENTAGE), parseDimension("1%"));
+	EXPECT_EQ(Dimension(1, DimensionType::LENGTH), parseDimension("1.0"));
+	EXPECT_EQ(Dimension(1, DimensionType::PERCENTAGE), parseDimension("1.0%"));
+	EXPECT_EQ(Dimension(1, DimensionType::PERCENTAGE), parseDimension("01.0%"));
+	EXPECT_EQ(Dimension(1, DimensionType::PERCENTAGE), parseDimension("0001.0%"));
+	EXPECT_EQ(Dimension(1, DimensionType::LENGTH), parseDimension("0001.0"));
+	EXPECT_EQ(Dimension(1, DimensionType::LENGTH), parseDimension("0001"));
+	EXPECT_EQ(Dimension(2, DimensionType::LENGTH), parseDimension("0002"));
+	EXPECT_THROW(parseDimension("a"), parseException);
+	EXPECT_THROW(parseDimension("0a1"), parseException);
+	EXPECT_THROW(parseDimension("01.a"), parseException);
+	EXPECT_THROW(parseDimension("01.a&"), parseException);
+	EXPECT_THROW(parseDimension("0.1.1"), parseException);
+	EXPECT_THROW(parseDimension("00000"), parseException);
+	EXPECT_THROW(parseDimension("0"), parseException);
+	EXPECT_THROW(parseDimension("%"), parseException);
+	EXPECT_THROW(parseDimension("."), parseException);
+	EXPECT_THROW(parseDimension("1.0e10"), parseException);
+	EXPECT_THROW(parseDimension("0.1"), parseException);
+	EXPECT_THROW(parseDimension("0.1%"), parseException);
+	EXPECT_THROW(parseDimension("0."), parseException);
+	EXPECT_THROW(parseDimension(".0"), parseException);
 }
 
 } /* namespace Numbers */
