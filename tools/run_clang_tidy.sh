2b75e167888d6e6b621eb9fdb150c9a9f56b0ba9
Mel McCalla <melmccalla@gmail.com>
2020-07-16 16:52:14 -0700

    Fixed ClangTidy and ClangSA

--- tools/run_clang_tidy.sh
+++ tools/run_clang_tidy.sh
@@ -3,7 +3,7 @@ set +o pipefail
 mkdir -p ${WORKSPACE}/build/Analysis/CodeChecker/ClangTidy
 cat ${WORKSPACE}/.codechecker_skip | sed "s|*/PROJECT_DIR|${WORKSPACE}|" > ${WORKSPACE}/build/Analysis/CodeChecker/ClangTidy/.codechecker_skip
 cat ${WORKSPACE}/build/Analysis/CodeChecker/ClangTidy/.codechecker_skip
-${CODECHECKER_PATH}/_CodeChecker analyze "${WORKSPACE}/build/Release/compile_commands.json" \
+${CODECHECKER_PATH}/_CodeChecker analyze "${WORKSPACE}/build/Analysis/tmp/ClangTidy/compile_commands.json" \
   -j6 \
   --analyzers clang-tidy \
   --enable-all \
--- tools/run_clangsa.sh
+++ tools/run_clangsa.sh
@@ -5,7 +5,7 @@ mkdir -p ${WORKSPACE}/build/Analysis/CodeChecker/ClangSA
 cat ${WORKSPACE}/.codechecker_skip | sed "s|*/PROJECT_DIR|${WORKSPACE}|" > ${WORKSPACE}/build/Analysis/CodeChecker/ClangSA/.codechecker_skip
 cat ${WORKSPACE}/build/Analysis/CodeChecker/ClangSA/.codechecker_skip
 ${CODECHECKER_PATH}/_CodeChecker analyze "${WORKSPACE}/build/Analysis/tmp/ClangSA/compile_commands.json" \
-  -j6 \
+  -j2 \
   --analyzers clangsa \
   --enable-all \
   --enable alpha \
