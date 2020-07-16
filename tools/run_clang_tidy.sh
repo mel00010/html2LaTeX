40164eddfb287cb027e293b1aa4f9f152107dceb
Mel McCalla <melmccalla@gmail.com>
2020-07-16 15:42:13 -0700

    Updated tool scripts to fix CodeChecker

--- tools/run_clang_tidy.sh
+++ tools/run_clang_tidy.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 set +o pipefail
-mkdir -p build/Analysis/CodeChecker/ClangTidy
-cat .codechecker_skip | sed "s|*/PROJECT_DIR|${WORKSPACE}|" > build/Analysis/CodeChecker/ClangTidy/.codechecker_skip
-${CODECHECKER_PATH}/_CodeChecker analyze "build/Analysis/tmp/ClangTidy/compile_commands.json" \
+mkdir -p ${WORKSPACE}/build/Analysis/CodeChecker/ClangTidy
+cat ${WORKSPACE}/.codechecker_skip | sed "s|*/PROJECT_DIR|${WORKSPACE}|" > ${WORKSPACE}/build/Analysis/CodeChecker/ClangTidy/.codechecker_skip
+${CODECHECKER_PATH}/_CodeChecker analyze "${WORKSPACE}/build/Analysis/tmp/ClangTidy/compile_commands.json" \
   -j6 \
   --analyzers clang-tidy \
   --enable-all \
-  -i build/Analysis/CodeChecker/ClangTidy/.codechecker_skip \
-  --output build/Analysis/CodeChecker/ClangTidy/
+  -i ${WORKSPACE}/build/Analysis/CodeChecker/ClangTidy/.codechecker_skip \
+  --output ${WORKSPACE}/build/Analysis/CodeChecker/ClangTidy/
--- tools/run_clangsa.sh
+++ tools/run_clangsa.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 set +o pipefail
-mkdir -p build/Analysis/CodeChecker/ClangSA
-cat .codechecker_skip | sed "s|*/PROJECT_DIR|${WORKSPACE}|" > build/Analysis/CodeChecker/ClangSA/.codechecker_skip
-${CODECHECKER_PATH}/_CodeChecker analyze "build/Analysis/tmp/ClangSA_CTU/compile_commands.json" \
+mkdir -p ${WORKSPACE}/build/Analysis/CodeChecker/ClangSA
+cat ${WORKSPACE}/.codechecker_skip | sed "s|*/PROJECT_DIR|${WORKSPACE}|" > ${WORKSPACE}/build/Analysis/CodeChecker/ClangSA/.codechecker_skip
+${CODECHECKER_PATH}/_CodeChecker analyze "${WORKSPACE}/build/Analysis/tmp/ClangSA_CTU/compile_commands.json" \
   -j6 \
   --analyzers clangsa \
   --enable-all \
   --enable alpha \
-  -i build/Analysis/CodeChecker/ClangSA/.codechecker_skip \
-  --output build/Analysis/CodeChecker/ClangSA/
+  -i ${WORKSPACE}/build/Analysis/CodeChecker/ClangSA/.codechecker_skip \
+  --output ${WORKSPACE}/build/Analysis/CodeChecker/ClangSA/
