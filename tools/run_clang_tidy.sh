cf847bbe410441db9c3e7ad6d61621da402ecd8a
Mel McCalla <melmccalla@gmail.com>
2020-07-16 16:05:29 -0700

    Updated tool scripts

--- tools/run_clang_tidy.sh
+++ tools/run_clang_tidy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-
 set +o pipefail
 mkdir -p ${WORKSPACE}/build/Analysis/CodeChecker/ClangTidy
 cat ${WORKSPACE}/.codechecker_skip | sed "s|*/PROJECT_DIR|${WORKSPACE}|" > ${WORKSPACE}/build/Analysis/CodeChecker/ClangTidy/.codechecker_skip
-${CODECHECKER_PATH}/_CodeChecker analyze "${WORKSPACE}/build/Analysis/tmp/ClangTidy/compile_commands.json" \
+cat ${WORKSPACE}/build/Analysis/CodeChecker/ClangTidy/.codechecker_skip
+${CODECHECKER_PATH}/_CodeChecker analyze "${WORKSPACE}/build/Release/compile_commands.json" \
   -j6 \
   --analyzers clang-tidy \
   --enable-all \
